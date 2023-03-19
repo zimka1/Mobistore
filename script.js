e07f336d625e6264c599451892fe39dae188a018
zimka1 <zimkail2015@mail.ru>
2023-03-19 17:35:27 +0100

    Новый блок
    Добавил новый блок testDr, но без адаптива

--- script.js
+++ script.js
@@ -22,17 +22,9 @@ menu2.onclick = function myFunction(){
   }
 }
 
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 5,
-    slidesPerView: 5,
-  });
-  var swiper2 = new Swiper(".mySwiper2", {
-    
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    thumbs: {
-      swiper: swiper,
-    },
-  });
+function clearTextarea(element) {
+  if (element.value == element.defaultValue) {
+    element.value = '';
+  }
+}
+
