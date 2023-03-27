df69ceb031ebe11ad1c1f84090b26a1b2f1571ef
zimka1 <zimkail2015@mail.ru>
2023-03-27 17:46:52 +0200

    Ответы блок Questions
    Добавил ответы в блок Questions

--- script.js
+++ script.js
@@ -247,3 +247,30 @@ $(document).ready(function(){
     }
  });
 });
+
+
+var acc = document.getElementsByClassName("accordionItemHeading");
+var i;
+var previous = null;
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+    if (previous !== null) {
+      previous.classList.remove("active");
+      var prevPanel = previous.nextElementSibling;
+      prevPanel.style.maxHeight = null;
+    }
+    if (previous == this){
+      previous = null;
+    } else{
+      previous = this;
+    }
+  });
+}
