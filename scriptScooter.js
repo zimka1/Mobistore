ab0ded6b6633a619eadea807670a63a2f100ac02
zimka1 <zimkail2015@mail.ru>
2023-04-18 21:51:17 +0200

    Убрал jquery в Scooter.html

--- scriptScooter.js
+++ scriptScooter.js
@@ -1,9 +1,43 @@
-// Слайдер на странице с товаром 
+// Слайдер feedback
 
+const slider2 = document.querySelector('.feedBack  .slider');
+const slides2 = slider2.querySelectorAll('.feedBack  .slide');
+const prevBtn2 = document.querySelector('.feedBack  .slider-prev');
+const nextBtn2 = document.querySelector('.feedBack  .slider-next');
+
+let currentSlide2 = 0;
+
+function goToSlide2(n) {
+  slider2.scrollLeft = slides2[n].offsetLeft;
+  currentSlide2 = n;
+}
+
+function nextSlide2() {
+  if (currentSlide2 === slides2.length - 4) {
+    goToSlide2(0);
+  } else {
+    goToSlide2(currentSlide2 + 1);
+  }
+}
+
+function prevSlide2() {
+  if (currentSlide2 === 0) {
+    goToSlide2(slides2.length - 1);
+  } else {
+    goToSlide2(currentSlide2 - 1);
+  }
+}
+
+
+prevBtn2.addEventListener('click', prevSlide2);
+nextBtn2.addEventListener('click', nextSlide2);
+
+goToSlide2(0);
+
+
+// Слайдер на странице с товаром 
 const slider3 = document.querySelector('.description-imeges .slider');
 const slides3 = slider3.querySelectorAll('.description-imeges .slide');
-const prevBtn3 = document.querySelector('.description-imeges .slider-prev');
-const nextBtn3 = document.querySelector('.description-imeges .slider-next');
 const dots3 = document.querySelectorAll('.description-imeges .slider-dot');
 
 let currentSlide3 = 0;
@@ -12,22 +46,6 @@ function goToSlide3(n) {
   slider3.scrollLeft = slides3[n].offsetLeft;
   currentSlide3 = n;
   setActiveDot3();
-}
-
-function nextSlide3() {
-  if (currentSlide3 === slides3.length - 1) {
-    goToSlide3(0);
-  } else {
-    goToSlide3(currentSlide3 + 1);
-  }
-}
-
-function prevSlide3() {
-  if (currentSlide3 === 0) {
-    goToSlide3(slides3.length - 1);
-  } else {
-    goToSlide3(currentSlide3 - 1);
-  }
 }
 
 function setActiveDot3() {
@@ -42,7 +60,8 @@ dots3.forEach((dot, index) => {
   });
 });
 
-prevBtn3.addEventListener('click', prevSlide3);
-nextBtn3.addEventListener('click', nextSlide3);
 
 goToSlide3(0);
+
+
+
